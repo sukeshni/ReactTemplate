d28f851d043aea1ae88cf42b1e30232b018bc396
sukeshni <sukeshni.kantrod@gmail.com>
2017-05-01 11:02:30 +0900

    Add App component

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    // Return your main outer components inside div 
+    return <div>Hello world!</div>;
+  }
+}
+
+export default App;
